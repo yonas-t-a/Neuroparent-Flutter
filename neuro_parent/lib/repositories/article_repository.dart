cca7a2b38d1e437dd9f389995df3508f60224561
yonas-t-a <yonas.tessema@aait.edu.et>
2025-06-10 23:25:52 +0300

    Defin article Repository class

---
+++ neuro_parent/lib/repositories/article_repository.dart
@@ -0,0 +1,11 @@
+import '../services/article_service.dart';
+import '../models/article.dart';
+import 'dart:typed_data'; // Import Uint8List
+
+class ArticleRepository {
+  final ArticleService articleService;
+
+  ArticleRepository({required this.articleService});
+
+  
+}
