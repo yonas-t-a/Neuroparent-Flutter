6cbdd4e508faeafaadbd778f8aa22981a6ed2766
yonas-t-a <yonas.tessema@aait.edu.et>
2025-06-10 23:26:39 +0300

    define article repositry methods

--- neuro_parent/lib/repositories/article_repository.dart
+++ neuro_parent/lib/repositories/article_repository.dart
@@ -7,5 +7,41 @@ class ArticleRepository {
 
   ArticleRepository({required this.articleService});
 
-  
+  Future<List<Article>> getArticles() => articleService.getArticles();
+  Future<Article> getArticleById(int id) => articleService.getArticleById(id);
+  Future<List<Article>> getArticlesByCategory(String category) =>
+      articleService.getArticlesByCategory(category);
+  Future<Article> createArticle({
+    required String title,
+    required String content,
+    required String category,
+    required Uint8List imageBytes,
+    required String filename,
+    required String contentType,
+  }) => articleService.createArticle(
+    title: title,
+    content: content,
+    category: category,
+    imageBytes: imageBytes,
+    filename: filename,
+    contentType: contentType,
+  );
+  Future<Article> updateArticle({
+    required int id,
+    String? title,
+    String? content,
+    String? category,
+    Uint8List? imageBytes,
+    String? filename,
+    String? contentType,
+  }) => articleService.updateArticle(
+    id: id,
+    title: title,
+    content: content,
+    category: category,
+    imageBytes: imageBytes,
+    filename: filename,
+    contentType: contentType,
+  );
+  Future<void> deleteArticle(int id) => articleService.deleteArticle(id);
 }
