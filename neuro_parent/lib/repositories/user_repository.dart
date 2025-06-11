18a22d052db5f94ba587b3515fc65c515748ef71
Soreti Oumar <167539467+soretiu12@users.noreply.github.com>
2025-06-11 11:08:53 +0300

    class

--- neuro_parent/lib/repositories/user_repository.dart
+++ neuro_parent/lib/repositories/user_repository.dart
@@ -1,3 +1,10 @@
 import '../services/user_service.dart';
 import '../models/user.dart';
 
+class UserRepository {
+  final UserService userService;
+
+  UserRepository({required this.userService});
+
+
+}
