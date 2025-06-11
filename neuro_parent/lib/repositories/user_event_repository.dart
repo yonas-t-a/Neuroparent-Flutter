74ccb0ec2b4660967eed7015d5746d0fc9c8be0c
Bezawit Geta <bezageta2@gmail.com>
2025-06-11 10:48:42 +0300

    Update user_event_repository.dart

--- neuro_parent/lib/repositories/user_event_repository.dart
+++ neuro_parent/lib/repositories/user_event_repository.dart
@@ -1,2 +1,11 @@
 import '../services/user_event_service.dart';
 import '../models/user_event.dart';
+
+
+class UserEventRepository {
+  final UserEventService userEventService;
+
+  UserEventRepository({required this.userEventService});
+
+  
+}
