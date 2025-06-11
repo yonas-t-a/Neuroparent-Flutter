565c80ed347ad6e8795bfb9a51ff8e27c03b1310
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-06-11 10:25:15 +0300

    chore: import Flutter material package for UI components

---
+++ neuro_parent/lib/user/articles/article_list.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:neuro_parent/user/widgets/user_bottom_nav.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../bloc/user_article_bloc.dart';
+import 'package:neuro_parent/core/categories.dart';
+
