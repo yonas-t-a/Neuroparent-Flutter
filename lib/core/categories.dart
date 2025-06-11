48d37090824956f27c2e26fecc9d495f836c4947
soretiu12 <soretiu24@gmail.com>
2025-06-11 07:06:08 +0300

    Add categories.dart

---
+++ lib/core/categories.dart
@@ -0,0 +1,14 @@
+class Categories {
+  static const List<String> allCategories = [
+    'All',
+    'ASD',
+    'ADHD',
+    'Dyslexia',
+    'Dyscalculia',
+    'Dyspraxia',
+    'Tourette Syndrome',
+    'OCD',
+    'Bipolar',
+    'Anxiety',
+  ];
+}
