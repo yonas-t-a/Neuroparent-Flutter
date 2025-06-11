db89ff401b633f61d02f784406b9bc4cba24e7b7
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-06-11 10:41:27 +0300

    articleState class created

--- neuro_parent/lib/user/bloc/user_article_bloc.dart
+++ neuro_parent/lib/user/bloc/user_article_bloc.dart
@@ -3,3 +3,23 @@ import '../../models/article.dart';
 import '../../repositories/article_repository.dart';
 import '../../services/article_service.dart';
 import '../../auth/auth_bloc.dart';
+
+class ArticleState {
+  final List<Article> articles;
+  final bool isLoading;
+  final String? error;
+
+  ArticleState({this.articles = const [], this.isLoading = false, this.error});
+
+  ArticleState copyWith({
+    List<Article>? articles,
+    bool? isLoading,
+    String? error,
+  }) {
+    return ArticleState(
+      articles: articles ?? this.articles,
+      isLoading: isLoading ?? this.isLoading,
+      error: error,
+    );
+  }
+}
