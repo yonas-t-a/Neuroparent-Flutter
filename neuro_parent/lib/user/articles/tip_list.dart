adab168c21454faf33573bc32834eff2c60b155d
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-06-11 10:30:37 +0300

    Tipsscreenstate class

--- neuro_parent/lib/user/articles/tip_list.dart
+++ neuro_parent/lib/user/articles/tip_list.dart
@@ -12,3 +12,25 @@ class TipsScreen extends ConsumerStatefulWidget {
   ConsumerState<TipsScreen> createState() => _TipsScreenState();
 }
 
+class _TipsScreenState extends ConsumerState<TipsScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  String _searchQuery = '';
+  String _selectedCategory = 'ALL';
+
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(
+      () => ref.read(userArticleProvider.notifier).fetchArticles(),
+    );
+  }
+
+  int _getCurrentIndex(BuildContext context) {
+    final location = GoRouterState.of(context).uri.toString();
+    if (location.startsWith('/home')) return 0;
+    if (location.startsWith('/events')) return 1;
+    if (location.startsWith('/registered')) return 2;
+    if (location.startsWith('/articles')) return 3;
+    if (location.startsWith('/profile')) return 4;
+    return 0;
+  }
