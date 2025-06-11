c6ccaff846fb971735f374faa184d9190f568891
Soreti Oumar <167539467+soretiu12@users.noreply.github.com>
2025-06-11 11:09:24 +0300

    user service complexity hiding functions

--- neuro_parent/lib/repositories/user_repository.dart
+++ neuro_parent/lib/repositories/user_repository.dart
@@ -6,5 +6,23 @@ class UserRepository {
 
   UserRepository({required this.userService});
 
+  Future<User> getUserById(int id) => userService.getUserById(id);
+
+  Future<User> updateUser(
+    int id, {
+    String? name,
+    String? email,
+    String? password,
+    String? oldPassword,
+    String? newPassword,
+  }) => userService.updateUser(
+    id,
+    name: name,
+    email: email,
+    password: password,
+    oldPassword: oldPassword,
+    newPassword: newPassword,
+  );
+
 
 }
